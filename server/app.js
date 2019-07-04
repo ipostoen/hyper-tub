2206639d42386aacdf729f13f78cc6f603fa6b56
Илья <i@devurai.com>
2019-07-04 22:49:54 +0300

    asdasd

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ var server = http.createServer(app);
 var corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  exposedHeaders: ['*'],
+  exposedHeaders: ['*']
 }
 
 app.set('trust proxy', true);
